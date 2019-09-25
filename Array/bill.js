9bfa9553d785d695bb09a8ab1b9421d2dd947af9
biadelmiro <delmirobia2015@gmail.com>
2019-09-25 16:30:15 -0300

    minhas alterações 01

--- Array/bill.js
+++ Array/bill.js
@@ -20,7 +20,11 @@ var bills = [50.23, 19.12, 34.01,
     100.11, 12.15, 9.90, 29.11, 12.99,
     10.00, 99.22, 102.20, 100.10, 6.77, 2.22
 ];
+console.log(bills);
 
 var total = bills.map(function(bill){
-    bill = bill+100/15;
+    var totals = bill + ((bill*15)/100);
+    console.log(totals.toFixed(2));
 }
+
+);
