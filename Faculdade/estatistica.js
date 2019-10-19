de55df4be13a2c1a9ce618d4e21b9166c5a04854
biadelmiro <delmirobia2015@gmail.com>
2019-10-19 14:56:53 -0300

    novas pastas e um arquivo contasBanco

---
+++ Faculdade/estatistica.js
@@ -0,0 +1,9 @@
+/* Fazer um array e encontrar a média e a mediana */
+
+let valor =[22,34,55,21,19,34,51,34,22];
+
+for (let a = 0; a <= length.valor; a++){
+    a = (a + valor)/length.valor;
+    console.log('valores', a);
+    return;
+}
